97d0ea616e3cd22edfdb7bd15172a2a83082b6f7
id2k1149 <id2k1149@gmail.com>
2024-10-03 12:07:18 -0400

    Add Portrait and Landscape

---
+++ V.swiftpm/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  Cube
+//
+//  Created by Max Franz Immelmann on 10/3/24.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
